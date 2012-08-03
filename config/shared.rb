594ac073cf6c06e97716e396f0ec344d601c0c91
Tommi Lew <cs.lew.1111@gmail.com>
2012-08-04 00:12:22 +0800

    Remove datamapper

--- config/shared.rb
+++ config/shared.rb
@@ -8,11 +8,4 @@ RACK_ENV ||= ENV["RACK_ENV"] || "development"
 require 'bundler/setup'
 require 'sinatra'
 require "sinatra/reloader" if development?
-require 'data_mapper'
 require 'haml'
-
-dbconfig = YAML.load(ERB.new(File.read("#{File.dirname(__FILE__)}/database.yml")).result)
-
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/#{dbconfig[RACK_ENV]['database']}")
-
-DataMapper.auto_upgrade!
