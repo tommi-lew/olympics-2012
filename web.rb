75443c3f283aa8912e86a113b52fa025ad4c8a6f
Tommi Lew <cs.lew.1111@gmail.com>
2012-08-06 00:54:42 +0800

    Added page to view countries with no medals
    Also
    -refactoring
    -added database

--- web.rb
+++ web.rb
@@ -5,11 +5,40 @@ require 'json'
 require 'curl'
 
 get '/' do
+  haml :index, :locals => {:medal_count => medal_count}
+end
+
+get '/all_countries' do
+  result = medal_count
+  countries_wo_medals = []
+
+  countries = Country.all
+  countries.each do |country|
+    countries_wo_medals << country if !has_country?(result, country.url_name)
+  end
+
+  haml :no_medals, :locals => { :countries => countries_wo_medals }
+end
+
+def medal_count
   curl = Curl::Easy.new("http://apify.heroku.com/api/olympics2012_medals.json")
   curl.perform
-  result = curl.body_str
+  result = JSON.parse(curl.body_str)
 
-  json_result = JSON.parse(result)
+  result.each do |country|
+    country["url"].slice!("/country/")
+    country["url"].slice!("/medals/index.html")
+  end
+  result
+end
 
-  haml :index, :locals => {:json_result => json_result}
+def has_country?(medal_count, country_url_name)
+  result = false
+  medal_count.each do |country_w_medal|
+    if country_w_medal["url"] == country_url_name
+      result = true
+      break
+    end
+  end
+  result
 end
