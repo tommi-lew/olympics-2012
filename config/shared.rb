fdab528484b95d66fc33e0764077153ae211a62d
Tommi Lew <cs.lew.1111@gmail.com>
2012-08-05 18:10:17 +0800

    Roll back DB configs: not using DB yet

--- config/shared.rb
+++ config/shared.rb
@@ -10,11 +10,3 @@ require 'data_mapper'
 
 Dir["#{File.dirname(__FILE__)}/../lib/**/*.rb"].each { |f| require f }
 Dir["#{File.dirname(__FILE__)}/../models/**/*.rb"].each { |f| require f }
-
-dbconfig = YAML.load(ERB.new(File.read("#{File.dirname(__FILE__)}/database.yml")).result)
-
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/#{dbconfig[RACK_ENV]['database']}")
-
-DataMapper.auto_upgrade!
-
-Dir["#{File.dirname(__FILE__)}/../db/**/*.rb"].each { |f| require f }
