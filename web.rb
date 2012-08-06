89ce260588a20ca83ad7e8447efb2781d5e0e90a
Tommi Lew <cs.lew.1111@gmail.com>
2012-08-06 16:30:24 +0800

    Added medal count by continent

--- web.rb
+++ web.rb
@@ -20,6 +20,35 @@ get '/no_medal' do
   haml :no_medal, :locals => { :countries => countries_wo_medals }
 end
 
+get '/medal_by_continent' do
+  result = medal_count
+
+  continent_medals = {
+    'africa' => {'bronze' => 0, 'silver' => 0, 'gold' => 0, 'total' => 0 },
+    'asia' => { 'bronze' => 0, 'silver' => 0, 'gold' => 0, 'total' => 0 },
+    'oceania' => { 'bronze' => 0, 'silver' => 0, 'gold' => 0, 'total' => 0 },
+    'americas' => { 'bronze' => 0, 'silver' => 0, 'gold' => 0, 'total' => 0 },
+    'europe' => { 'bronze' => 0, 'silver' => 0, 'gold' => 0, 'total' => 0 }
+  }
+
+  result.each do |country|
+    continent = country['continent']
+    continent_medals[continent]['bronze'] = continent_medals[continent]['bronze'] + country['bronze'].to_i
+    continent_medals[continent]['silver'] = continent_medals[continent]['silver'] + country['silver'].to_i
+    continent_medals[continent]['gold'] = continent_medals[continent]['gold'] + country['gold'].to_i
+    continent_medals[continent]['total'] = continent_medals[continent]['total'] +
+      country['bronze'].to_i +
+      country['silver'].to_i +
+      country['gold'].to_i
+  end
+
+  continent_medals_array = []
+  continent_medals.each { |k, v| continent_medals_array << v.merge!({ 'continent' => k }) }
+  continent_medals_array.sort_by! { |o| -o['total'] }
+
+  haml :medal_count_by_continent, :locals => { :continent_medals => continent_medals_array }
+end
+
 def medal_count
   curl = Curl::Easy.new("http://apify.heroku.com/api/olympics2012_medals.json")
   curl.perform
